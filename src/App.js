6f6f12742b9cb84dba091fafeea43f54f39e65a0
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-09-20 15:49:03 +0200

    added the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components/index';
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <h1>App Component</h1>
+    <div className="app">
+        <div className="navbar">
+            <Navbar />
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer">
+
+        </div>
     </div>
   );
 }
